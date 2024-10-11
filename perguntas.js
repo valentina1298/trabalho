f55db0c459655c54047053642a9112683c06b162
valentina1298 <133918293+valentina1298@users.noreply.github.com>
2024-10-11 09:34:43 -0300

    ytrifgonmhogh

---
+++ perguntas.js
@@ -0,0 +1,98 @@
+criarCartao(
+    'Matematica',
+    'No meu jardim existem 3 pés de alface, 1 de pepino e 5 de cenoura. Quantos pés eu tenho no total?',
+    'Eu tenho dois pés'
+)
+
+
+criarCartao(
+    'Matematica',
+    'Lucas tem na sua coleção 54 bolinhas de gude que ele usa para disputar divertidas partidas com seus amigos. Destas 54, 1/6 são azuis. Quantas bolinhas são verdes?',
+    '45 bolinhas são verdes.'
+)
+
+criarCartao(
+    'Biologia',
+    'Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    ' Entre 4 a 6 litros. São retirados 450 mililitros.'
+)
+
+criarCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
+)
+
+criarCartao(
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia'
+)
+
+criarCartao(
+    'Historia',
+    ' Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart'
+)
+
+criarCartao(
+    'Matematica',
+    ' Quantas casas decimais tem o número pi?',
+    'Infinitas'
+)
+
+criarCartao(
+    'Quimica',
+    ' Atualmente, quantos elementos químicos a tabela periódica possui?',
+    ' 118'
+)
+
+criarCartao(
+    'Geografia',
+    ' Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+    ' Japão e Serra Leoa'
+)
+
+criarCartao(
+     'Historia',
+    ' Quais as duas datas que são comemoradas em novembro?',
+    ' Proclamação da República e Dia Nacional da Consciência Negra.'
+)
+
+criarCartao(
+    'Artes',
+   ' Quem pintou "Guernica"?',
+   '  Pablo Picasso'
+)
+
+criarCartao(
+    'Historia',
+   ' Em que período da pré-história o fogo foi descoberto?',
+   '  Paleolítico'
+)
+
+criarCartao(
+    'Geografia',
+   ' Qual a montanha mais alta do Brasil?',
+   '  Pico da Neblina'
+)
+
+criarCartao(
+    'Portugues',
+   ' Como é a conjugação do verbo caber na 1.ª pessoa do singular do presente do indicativo?',
+   ' Eu caibo'
+)
+
+criarCartao(
+    'Biologia',
+   ' Quais doenças são sexualmente transmissíveis?',
+   ' Gonorreia, clamídia e sífilis'
+)
+
+criarCartao(
+    'Biologia',
+   ' Quais doenças são sexualmente transmissíveis?',
+   ' Gonorreia, clamídia e sífilis'
+)
+
+
