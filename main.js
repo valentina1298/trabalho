dfe1547207d1fb841f34007597eed717e6321472
valentina1298 <133918293+valentina1298@users.noreply.github.com>
2024-10-25 11:10:06 -0300

    dggdfd

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function criarCartao (categoria, pergunta, resposta) {
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
     </div>
-    <div class="resposta-carto">
+    <div class="resposta-cartao">
         <p>${resposta}</p>
     </div>
    </div>
